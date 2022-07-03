aab994979b0587965b86609a61dc77e037ee8ca3
Jeircho <jericho_ph@qq.com>
2022-07-03 23:28:43 +0800

    修改种类字段：是否显示→是否启用

--- model/FactoryModel.go
+++ model/FactoryModel.go
@@ -2,9 +2,9 @@ package model
 
 type FactoryModel struct {
 	BaseModel
-	UniqueCode      string                `gorm:"type:CHAR(5);UNIQUE;NOT NULL;COMMENT:生产厂家代码;" json:"unique_code"`
-	Name            string                `gorm:"type:VARCHAR(64);UNIQUE;NOT NULL;COMMENT:生产厂家名称;" json:"name"`
-	ShotName        string                `gorm:"type:VARCHAR(64);COMMENT:生产厂家名称;" json:"shot_name"`
+	UniqueCode      string                `gorm:"type:CHAR(5);UNIQUE;NOT NULL;COMMENT:供应商代码;" json:"unique_code"`
+	Name            string                `gorm:"type:VARCHAR(64);UNIQUE;NOT NULL;COMMENT:供应商名称;" json:"name"`
+	ShotName        string                `gorm:"type:VARCHAR(64);COMMENT:供应商简称;" json:"shot_name"`
 	EntireInstances []EntireInstanceModel `gorm:"constraint:OnUpdate:CASCADE;foreignKey:FactoryUniqueCode;references:UniqueCode;COMMENT:相关器材;" json:"entire_instances"`
 }
 
