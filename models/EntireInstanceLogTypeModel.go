480c43a3fc30dce2d2de02c9b897d87c12d8730a
jericho <jericho_ph@qq.com>
2022-08-17 15:30:09 +0800

    日志新增关联：线别、站场、区间、道口、中心

--- models/EntireInstanceLogTypeModel.go
+++ models/EntireInstanceLogTypeModel.go
@@ -3,11 +3,11 @@ package models
 // EntireInstanceLogTypeModel 器材日志类型模型
 type EntireInstanceLogTypeModel struct {
 	BaseModel
-	UniqueCode             string `gorm:"type:VARCHAR(64);UNIQUE;NOT NULL;DEFAULT;COMMENT:器材日志类型代码;" json:"unique_code"`
-	Name                   string `gorm:"type:VARCHAR(64);NOT NULL;DEFAULT;COMMENT:器材日志类型名称;" json:"name"`
-	UniqueCodeForParagraph string `gorm:"type:VARCHAR(64);UNIQUE;NOT NULL;DEFAULT;COMMENT:器材日志对应段中心代码;" json:"unique_code_for_paragraph"`
-	Number                 string `gorm:"type:VARCHAR(64);NOT NULL;DEFAULT;COMMENT:器材日志类型数字代码;" json:"number"`
-	Icon                   string `gorm:"type:VARCHAR(64);NOT NULL;DEFAULT;;COMMENT:器材日志类型图标;" json:"icon"`
+	UniqueCode             string `gorm:"type:VARCHAR(64);UNIQUE;NOT NULL;DEFAULT:;COMMENT:器材日志类型代码;" json:"unique_code"`
+	Name                   string `gorm:"type:VARCHAR(64);NOT NULL;DEFAULT:;COMMENT:器材日志类型名称;" json:"name"`
+	UniqueCodeForParagraph string `gorm:"type:VARCHAR(64);NOT NULL;DEFAULT:;COMMENT:器材日志对应段中心代码;" json:"unique_code_for_paragraph"`
+	Number                 string `gorm:"type:VARCHAR(64);NOT NULL;DEFAULT:;COMMENT:器材日志类型数字代码;" json:"number"`
+	Icon                   string `gorm:"type:VARCHAR(64);NOT NULL;DEFAULT:;COMMENT:器材日志类型图标;" json:"icon"`
 }
 
 // TableName 表名称
