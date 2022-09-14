0b19c26a0a2860bb0c2d57fa77fc4742a6cede03
Jeircho <jericho_ph@qq.com>
2022-09-15 00:01:53 +0800

    关闭数据迁移

--- models/EntireInstanceStatusModel.go
+++ models/EntireInstanceStatusModel.go
@@ -2,9 +2,9 @@ package models
 
 type EntireInstanceStatusModel struct {
 	BaseModel
-	UniqueCode      string                 `gorm:"type:VARCHAR(64);COMMENT:状态代码;" json:"unique_code"`
-	Name            string                 `gorm:"type:VARCHAR(64);COMMENT:状态名称;" json:"name"`
-	Number          string                 `gorm:"type:CHAR(2);COMMENT:状态数字代码;" json:"number"`
+	UniqueCode string `gorm:"type:VARCHAR(64);COMMENT:状态代码;" json:"unique_code"`
+	Name       string `gorm:"type:VARCHAR(64);COMMENT:状态名称;" json:"name"`
+	NumberCode string `gorm:"type:CHAR(2);COMMENT:状态数字代码;" json:"number_code"`
 }
 
 // TableName 表名称
